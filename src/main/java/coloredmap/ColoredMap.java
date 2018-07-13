dacf06de54f83fb89a5af54870c94be55388eb12
Tim Reinagel <tmr2_handbell@yahoo.com>
2018-07-12 20:54:42 -0700

    Fixed initialization bug, added update info, replaced deprecated code usage.

--- src/main/java/coloredmap/ColoredMap.java
+++ src/main/java/coloredmap/ColoredMap.java
@@ -6,35 +6,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.helpers.Prefs;
 import com.megacrit.cardcrawl.helpers.SaveHelper;
-import com.megacrit.cardcrawl.rooms.*;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
+@SpireInitializer
 public class ColoredMap implements PostInitializeSubscriber {
     private static final String MODNAME = "Colored Map";
-    private static final String AUTHOR = "t-larson";
-    private static final String DESCRIPTION = "v1.2.1";
-    
-    @SuppressWarnings("unused")
+    private static final String AUTHOR = "timeracers, t-larson";
+    private static final String DESCRIPTION = "v1.3";
+
     public ColoredMap() {
-        BaseMod.subscribeToPostInitialize(this);
+        BaseMod.subscribe(this);
     }
-    
-    @SuppressWarnings("unused")
+
     public static void initialize() {
-        ColoredMap cm = new ColoredMap();
+        new ColoredMap();
     }
-    
+
+    @Override
     public void receivePostInitialize() {
         Prefs modPrefs = SaveHelper.getPrefs("ColoredMapPrefs");
         
         ModPanel settingsPanel = new ModPanel();
         settingsPanel.state.put("selection", -1);
-        
-        //ModLabel instructions = 
+
         ModImage background = new ModImage(451.0f, 456.0f, "img/IconBackground.png");
-        settingsPanel.addImage(background);
+        settingsPanel.addUIElement(background);
         
         ModColorDisplay[] icons = new ModColorDisplay[6];
         ModSlider redSlider = new ModSlider("R", 350.0f, 700.0f, 255.0f, "", settingsPanel, (me) -> {
@@ -45,7 +45,7 @@ public class ColoredMap implements PostInitializeSubscriber {
                 modPrefs.flush();
             }
         });
-        settingsPanel.addSlider(redSlider);
+        settingsPanel.addUIElement(redSlider);
         
         ModSlider greenSlider = new ModSlider("G", 350.0f, 650.0f, 255.0f, "", settingsPanel, (me) -> {
             int selection = settingsPanel.state.get("selection");
@@ -55,7 +55,7 @@ public class ColoredMap implements PostInitializeSubscriber {
                 modPrefs.flush();
             }
         });
-        settingsPanel.addSlider(redSlider);
+        settingsPanel.addUIElement(redSlider);
         
         ModSlider blueSlider = new ModSlider("B", 350.0f, 600.0f, 255.0f, "", settingsPanel, (me) -> {
             int selection = settingsPanel.state.get("selection");
@@ -65,7 +65,7 @@ public class ColoredMap implements PostInitializeSubscriber {
                 modPrefs.flush();
             }
         });
-        settingsPanel.addSlider(greenSlider);
+        settingsPanel.addUIElement(greenSlider);
         
         ModSlider outlineSlider = new ModSlider("Outline", 350.0f, 550.0f, 100.0f, "%", settingsPanel, (me) -> {
             int selection = settingsPanel.state.get("selection");
@@ -75,7 +75,7 @@ public class ColoredMap implements PostInitializeSubscriber {
                 modPrefs.flush();
             } 
         });
-        settingsPanel.addSlider(blueSlider);
+        settingsPanel.addUIElement(blueSlider);
         
         icons[0] = new ModColorDisplay(800.0f, 625.0f, ImageMaster.MAP_NODE_ENEMY, ImageMaster.MAP_NODE_ENEMY_OUTLINE, (me) -> {
             settingsPanel.state.put("selection", 0);
@@ -84,7 +84,7 @@ public class ColoredMap implements PostInitializeSubscriber {
             blueSlider.setValue(modPrefs.getFloat("b_icon_0", 1.0f));
             outlineSlider.setValue(modPrefs.getFloat("a_icon_0", 1.0f));
         });
-        settingsPanel.addSlider(outlineSlider);
+        settingsPanel.addUIElement(outlineSlider);
         
         icons[1] = new ModColorDisplay(625.0f, 625.0f, ImageMaster.MAP_NODE_ELITE, ImageMaster.MAP_NODE_ELITE_OUTLINE, (me) -> {
             settingsPanel.state.put("selection", 1);
@@ -131,7 +131,7 @@ public class ColoredMap implements PostInitializeSubscriber {
             icons[i].g = modPrefs.getFloat("g_icon_" + i, 1.0f);
             icons[i].b = modPrefs.getFloat("b_icon_" + i, 1.0f);
             icons[i].aOutline = modPrefs.getFloat("a_icon_" + i, 1.0f);
-            settingsPanel.addColorDisplay(icons[i]);
+            settingsPanel.addUIElement(icons[i]);
         }
         
         Texture badgeTexture = new Texture(Gdx.files.internal("img/ColoredMapBadge.png"));
@@ -141,20 +141,27 @@ public class ColoredMap implements PostInitializeSubscriber {
     public static void setIconOutlineColor(AbstractRoom room, SpriteBatch sb) {
         Prefs modPrefs = SaveHelper.getPrefs("ColoredMapPrefs");
         float a = 0.0f;
-        
-        if (room.getMapSymbol() == "E") {
-            a = modPrefs.getFloat("a_icon_1", 1.0f);
-        } else if (room.getMapSymbol() == "M") {
-            a = modPrefs.getFloat("a_icon_0", 1.0f);
-        } else if (room.getMapSymbol() == "$") {
-            a = modPrefs.getFloat("a_icon_2", 1.0f);
-        } else if (room.getMapSymbol() == "R") {
-            a = modPrefs.getFloat("a_icon_3", 1.0f);
-        } else if (room.getMapSymbol() == "T") {
-            a = modPrefs.getFloat("a_icon_4", 1.0f);
-        } else if (room.getMapSymbol() == "?") {
-            a = modPrefs.getFloat("a_icon_5", 1.0f);
-        } 
+
+        switch (room.getMapSymbol()) {
+            case "E":
+                a = modPrefs.getFloat("a_icon_1", 1.0f);
+                break;
+            case "M":
+                a = modPrefs.getFloat("a_icon_0", 1.0f);
+                break;
+            case "$":
+                a = modPrefs.getFloat("a_icon_2", 1.0f);
+                break;
+            case "R":
+                a = modPrefs.getFloat("a_icon_3", 1.0f);
+                break;
+            case "T":
+                a = modPrefs.getFloat("a_icon_4", 1.0f);
+                break;
+            case "?":
+                a = modPrefs.getFloat("a_icon_5", 1.0f);
+                break;
+        }
         
         sb.setColor(new Color(0.0f, 0.0f, 0.0f, a));
     }
@@ -164,31 +171,38 @@ public class ColoredMap implements PostInitializeSubscriber {
         float r = 0.0f;
         float g = 0.0f;
         float b = 0.0f;
-        
-        if (room.getMapSymbol() == "E") {
-            r = modPrefs.getFloat("r_icon_1", 1.0f);
-            g = modPrefs.getFloat("g_icon_1", 1.0f);
-            b = modPrefs.getFloat("b_icon_1", 1.0f);
-        } else if (room.getMapSymbol() == "M") {
-            r = modPrefs.getFloat("r_icon_0", 1.0f);
-            g = modPrefs.getFloat("g_icon_0", 1.0f);
-            b = modPrefs.getFloat("b_icon_0", 1.0f);
-        } else if (room.getMapSymbol() == "$") {
-            r = modPrefs.getFloat("r_icon_2", 1.0f);
-            g = modPrefs.getFloat("g_icon_2", 1.0f);
-            b = modPrefs.getFloat("b_icon_2", 1.0f);
-        } else if (room.getMapSymbol() == "R") {
-            r = modPrefs.getFloat("r_icon_3", 1.0f);
-            g = modPrefs.getFloat("g_icon_3", 1.0f);
-            b = modPrefs.getFloat("b_icon_3", 1.0f);
-        } else if (room.getMapSymbol() == "T") {
-            r = modPrefs.getFloat("r_icon_4", 1.0f);
-            g = modPrefs.getFloat("g_icon_4", 1.0f);
-            b = modPrefs.getFloat("b_icon_4", 1.0f);
-        } else if (room.getMapSymbol() == "?") {
-            r = modPrefs.getFloat("r_icon_5", 1.0f);
-            g = modPrefs.getFloat("g_icon_5", 1.0f);
-            b = modPrefs.getFloat("b_icon_5", 1.0f);
+
+        switch (room.getMapSymbol()) {
+            case "E":
+                r = modPrefs.getFloat("r_icon_1", 1.0f);
+                g = modPrefs.getFloat("g_icon_1", 1.0f);
+                b = modPrefs.getFloat("b_icon_1", 1.0f);
+                break;
+            case "M":
+                r = modPrefs.getFloat("r_icon_0", 1.0f);
+                g = modPrefs.getFloat("g_icon_0", 1.0f);
+                b = modPrefs.getFloat("b_icon_0", 1.0f);
+                break;
+            case "$":
+                r = modPrefs.getFloat("r_icon_2", 1.0f);
+                g = modPrefs.getFloat("g_icon_2", 1.0f);
+                b = modPrefs.getFloat("b_icon_2", 1.0f);
+                break;
+            case "R":
+                r = modPrefs.getFloat("r_icon_3", 1.0f);
+                g = modPrefs.getFloat("g_icon_3", 1.0f);
+                b = modPrefs.getFloat("b_icon_3", 1.0f);
+                break;
+            case "T":
+                r = modPrefs.getFloat("r_icon_4", 1.0f);
+                g = modPrefs.getFloat("g_icon_4", 1.0f);
+                b = modPrefs.getFloat("b_icon_4", 1.0f);
+                break;
+            case "?":
+                r = modPrefs.getFloat("r_icon_5", 1.0f);
+                g = modPrefs.getFloat("g_icon_5", 1.0f);
+                b = modPrefs.getFloat("b_icon_5", 1.0f);
+                break;
         }
         
         sb.setColor(new Color(r, g, b, 1.0f));
