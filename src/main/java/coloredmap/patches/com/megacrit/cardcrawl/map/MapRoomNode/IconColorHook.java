6b42b9ab88658bd02aa72d041a57ea06afd25265
Tim Reinagel <tmr2_handbell@yahoo.com>
2018-09-28 19:19:52 -0700

    Updated Patch Locators, this fixes issues with the newest version weekly 43.

--- src/main/java/coloredmap/patches/com/megacrit/cardcrawl/map/MapRoomNode/IconColorHook.java
+++ src/main/java/coloredmap/patches/com/megacrit/cardcrawl/map/MapRoomNode/IconColorHook.java
@@ -2,14 +2,26 @@ package coloredmap.patches.com.megacrit.cardcrawl.map.MapRoomNode;
 
 import coloredmap.ColoredMap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+
+import java.util.ArrayList;
 
 @SpirePatch(cls="com.megacrit.cardcrawl.map.MapRoomNode", method="render")
 public class IconColorHook {
-    @SpireInsertPatch(loc=442, localvars={"room"})
+    @SpireInsertPatch(locator = Locator.class, localvars={"room"})
     public static void Insert(Object meObj, Object sbObj, Object roomObj) {
         ColoredMap.setIconColor((AbstractRoom)roomObj, (SpriteBatch)sbObj);
+    }
+
+    private static class Locator extends SpireInsertLocator {
+        public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(
+                "com.badlogic.gdx.graphics.g2d.SpriteBatch", "draw");
+            return new int[] { LineFinder.findAllInOrder(ctMethodToPatch, new ArrayList<>(), finalMatcher)[1] };
+        }
     }
 }
